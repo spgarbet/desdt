42e9f86ce5ead1f66cf371115301a6d752ed6922
Shawn Garbett <shawn@garbett.org>
2022-07-08 11:10:58 -0500

    Fully working Sick/Sicker in data.table

--- main.R
+++ main.R
@@ -13,6 +13,8 @@
 
 library(simmer)
 library(data.table)
+library(tidyr)
+
 
 reps    <- 1:5
 Ns      <- 10^(2:3)  # For development
@@ -20,40 +22,34 @@ models  <- c("sick_sicker")
 methods <- c("data.table", "base", "simmer")
 result  <- NA
 
-basedir <- getwd()
+result <- as.data.frame(t(
+  apply(
+    expand.grid(1:length(models), 1:length(methods), Ns, reps),
+    1,
+    function(x)
+    {
+      N        <- x[3]
+      method   <- methods[x[2]]
+      timing   <- c(NA, NA, NA)
+      work_env <- new.env()
 
-tryCatch(
-{
-  result <- as.data.frame(t(
-    apply(
-      expand.grid(1:length(models), 1:length(methods), Ns, reps),
-      1,
-      function(x)
-      {
-        N      <- x[3]
-        model  <- models[x[1]]
-        method <- methods[x[2]]
-        timing <- c(NA, NA, NA)
-        setwd(model)
-        work_env <- new.env()
-        source(paste0(method, ".R"), local=work_env)
-        timing  <- system.time(work_env$result <- work_env$simulation(N))
-        timing2 <- system.time(work_env$summarize(work_env$result))
-        setwd("..")
-        rm(work_env)
+      source(file.path(models[x[1]], "model.R"), local=work_env)
+      source(file.path(models[x[1]], paste0(method, ".R")), chdir=TRUE, local=work_env)
 
-        c(x, timing[1:3], timing2[4:6])
-      }
-    )
-  ))
-  names(result) <- c("model", "method", "N", "rep",
-                     "run.user", "run.sys", "run.elapsed",
-                     "sum.user", "sum.sys", "sum.elapsed")
-  result$model  <- factor(result$model,  levels=1:length(models),  labels=models)
-  result$method <- factor(result$method, levels=1:length(methods), labels=methods)
-},
- error=function(e) {setwd(basedir); e}
-)
+      timing  <- system.time(work_env$result <- work_env$simulation(N))
+      timing2 <- system.time(work_env$summarize(work_env$result))
+      work_env <- NULL
+
+      c(x, timing[1:3], timing2[4:6])
+    }
+  )
+))
+names(result) <- c("model", "method", "N", "rep",
+                   "run.user", "run.sys", "run.elapsed",
+                   "sum.user", "sum.sys", "sum.elapsed")
+result$model  <- factor(result$model,  levels=1:length(models),  labels=models)
+result$method <- factor(result$method, levels=1:length(methods), labels=methods)
+
 
 boxplot(run.user~model+method+N, result)
 
--- sick_sicker/data.table.R
+++ sick_sicker/data.table.R
@@ -7,7 +7,6 @@
 ##
 ## MIT License
 ##
-source("model.R")
 
 # Next state vectors
 v_when_H  <- c("S1","D")
@@ -60,11 +59,37 @@ draw_next_state <- function(dt_current, v_t_HD, event_no)
 }
 
 
+# Costs function:
+# calculates the cost accrued by an individual this cycle
+Costs <- function(M_t) {
+  # M_t: current health state
+  c_t <- c()
+  c_t[M_t == "H"]  <- c_H   # costs accrued by being Healthy this cycle
+  c_t[M_t == "S1"] <- c_S1   # costs accrued by being Sick this cycle
+  c_t[M_t == "S2"] <- c_S2   # costs at Dead state
+  c_t[M_t == "D"]  <- c_D
+
+  return(c_t)  # return costs accrued this cycle
+}
+# Effs function:
+# calculates QALYs accrued by an individual for this cycle
+Effs <- function(M_t) {
+  # M_t: current health state
+  q_t <- c()
+  q_t[M_t == "H"]  <- u_H  # QALYs accrued by being Healthy this cycle
+  q_t[M_t == "S1"] <- u_S1  # QALYs accrued by being Sick this cycle
+  q_t[M_t == "S2"] <- u_S2  # QALYs at Dead state
+  q_t[M_t == "D"]  <- u_D  # QALYs at Dead state
+
+  return(q_t)  # return the QALYs accrued this cycle
+}
+
+
 simulation <- function(N)
 {
   #return(replicate(N, rnorm(100)))
 
-  attach(params)
+  dt_demog <- NA
 
   dt_demog <- data.table(
     id      = 1:N,
@@ -99,4 +124,111 @@ simulation <- function(N)
   dt_demog
 }
 
-summarize <- function(result) replicate(nrow(result), rnorm(100))
+summarize <- function(dt_demog)
+{
+  N <- nrow(dt_demog)
+
+  #Matrix fo States, Costs, and Effs
+  m_M_D <- m_C_D <- m_E_D <-  matrix(nrow = N, ncol = n_t + 1,
+                               dimnames = list(paste("ind", 1:N, sep = " "),
+                                               paste("cycle", 0:n_t, sep = " ")))
+
+  #Change to long format for events
+  dt_demog_long_event <- pivot_longer(dt_demog, cols = starts_with("E_"),
+                                      names_to = c("State", "event"),
+                                      names_pattern =  "(\\w+)\\_(\\d+)") %>%
+    select(c("id", "event", "value")) %>% rename(State = value)
+
+  #Change to long format for time to event
+  dt_demog_long_time <- pivot_longer(dt_demog, cols = starts_with("timeE_"),
+                                     names_to = c("time", "event"),
+                                     names_pattern =  "(\\w+)\\_(\\d+)") %>%
+    select(c("id","event","value")) %>% rename(t_Event = value)
+
+  #Inner join
+  dt_demog_long <- inner_join(dt_demog_long_event,dt_demog_long_time, by = c("id","event") )
+  dt_demog_long <- data.table(dt_demog_long)
+  dt_demog_long <- dt_demog_long[t_Event <= 30,]    # Keep only events under 30 cycles
+
+  # Difference in cycles between events
+  dt_demog_long <- dt_demog_long[ , delta :=  shift(t_Event, type = "lead") - t_Event , by = id]
+  dt_demog_long <- dt_demog_long[is.na(delta), delta :=  (n_t + 1) - t_Event , by = id]
+
+  # Expansion of the States vector
+  dt_demog_expanded <- dt_demog_long[ ,list(freq=rep(State,delta))]
+  dt_demog_expanded <- as.matrix(dt_demog_expanded)
+
+  ## Transforming the expanded vector to State matrix
+  for (i in 1:N)
+  {
+    ini <- 31*(i-1) + 1
+    end <- ini + 30
+    m_M_D[i,] <- dt_demog_expanded[ini:end,]
+  }
+
+  # Drop auxiliar tables
+  dt_demog_expanded   <- NULL
+  dt_demog_long_event <- NULL
+  dt_demog_long_time  <- NULL
+  dt_demog_long       <- NULL
+
+
+  # Initializes the progress bar
+  # pb <- txtProgressBar(min = 1,      # Minimum value of the progress bar
+  #                      max = n_i, # Maximum value of the progress bar
+  #                      style = 3,    # Progress bar style (also available style = 1 and style = 2)
+  #                      width = 50,   # Progress bar width. Defaults to getOption("width")
+  #                      char = "=")   # Character used to create the bar
+  #
+  # for (ind in 1:n_i) {
+  # v_state_i <- c()
+  #
+  # for (i in 0:(n_events-1)) {
+  #
+  #   str_state <- paste("State <- as.character(dt_demog[ind,E_",i,"])", sep = "")
+  #   eval(parse(text = str_state))
+  #
+  #   str_tend <- paste("t_end <- as.numeric(dt_demog[ind,timeE_",(i+1),"])", sep = "")
+  #   eval(parse(text = str_tend))
+  #
+  #   str_tini <- paste("t_ini <- as.numeric(dt_demog[ind,timeE_",(i),"])", sep = "")
+  #   eval(parse(text = str_tini))
+  #
+  #   v_state <- rep(State, (t_end-t_ini))
+  #
+  #   v_state_i <- append(v_state_i,v_state)
+  #
+  # }
+  #
+  # #v_state_i
+  # m_M_D[ind,] <- v_state_i[1:31]
+  # setTxtProgressBar(pb, ind)
+  # }
+
+  # Estimation of Costs and effectiveness
+
+
+  for (t in 1:(n_t+1)) {
+
+    # Costs
+    m_C_D[, t] <- Costs(m_M_D[, t])  # costs accrued by each individual during cycle  t+1
+
+    # QALYs
+    m_E_D[, t] <- Effs(m_M_D[, t])  # QALYs accrued by each individual during cycle  t+1
+
+  }
+
+  ##### ================= CACLULATE TOTAL COSTS AND QALYS   ================#####
+
+  v_dw <- 1 / (1 + d_c) ^ (0:n_t) # calculate discount weight for each cycle based on discount rate d_r
+
+  tc_DES <- m_C_D %*% v_dw    # total discounted cost per individual
+  te_DES <- m_E_D %*% v_dw    # total discounted QALYs per individual
+
+  tc_avg_DES <- mean(tc_DES)    # average discounted cost
+  tc_se_DES  <- sd(tc_DES) / sqrt(N)
+  te_avg_DES <- mean(te_DES)    # average discounted QALYs
+  te_se_DES  <- sd(te_DES) / sqrt(N)
+
+  list(cost=tc_avg_DES, cost_se=tc_se_DES, qaly=te_avg_DES, qaly_se=te_se_DES)
+}
